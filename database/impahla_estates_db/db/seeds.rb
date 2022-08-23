6f94fef4fb60d9caf05f2d58d2a9d586262c1ca1
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2022-08-23 13:39:07 -0400

    Added the rest of the basic information for the seeding and wondering if I should and a media attribute to the Events table. Should I use active storage or cloudinary for the media and hold back on that? Also, how am I going to get reactions to work? As a string or as a integer? Many questions and about to try out a couple theories.

--- database/impahla_estates_db/db/seeds.rb
+++ database/impahla_estates_db/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying Everything"
+User.destroy_all
+Listing.destroy_all
+Event.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+puts "----Creating Users----"
+#User.create(username: 'kingofkings', email: 'mike@gmail.com', password: 'MineNotYours')
+mike = User.create(username: 'kingofkings', password: 'Mike123')
+jared = User.create(username: 'basetankgod', password: 'Jared123')
+larry = User.create(username: 'thabigslo', password: 'Larry123')
+ovan = User.create(username: 'onemagic19', password: 'Ovan123')
+rod = User.create(username: 'roddygeez', password: 'Rod123')
+
+puts "----Create Listings----"
+# Listing.create(address:'', description:'', price:'', bedroom:'', bath:'')
+mt_vernon_1 = Listing.create(address:'167 Clove Road, Mount Vernon, NY, 10550', description:'With some of the nations most prestigious neigherboring private schools and right outside NYC.', price:'853,000', bedroom:4, bath:8)
+
+puts "----Create Events----"
+# Event.create(text:'', date:'', media:'')
+nyc_event = Event.create(text:'NYC Real Estate Networking (Commercial & Residential) - 3rd Tuesday Morning' date:'08/23/2022', media:'http://www.centurynyceb5.com/siteberdev/wp-content/uploads/2018/06/669598864244571586.jpg')
+
+puts "----Create Posts----"
+# Post.create(text:'', media:'', reaction:'')
+first_home = Post.create(author: mike, text:'After a long year of searching my wife and I finally secured the home we\'ve been looking for!', media:'https://cdn-1.eneighborhoods.com/x2/@v=-513556722@/78091/8/872/M00000490-H5066872/M00000490-H5066872_1.jpg', reaction:'')
+
+puts "----Create Comments----"
+# Comment.create(text:'', media:'', reaction:'')
+1st_post = Comment.create(post: first_home, text:'I\'m so happy for you and congradulations!', media:'', reaction:'')
